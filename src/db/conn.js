0ee1fdc91714cfa99b2d13e34e81c0e227002d5d
abhay-raj-au50 <111737317+abhay-raj-au50@users.noreply.github.com>
2023-03-21 05:34:29 +0530

    Update conn.js

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 //creating a database
-mongoose.connect("mongodb://localhost:27017/abhay", {
+mongoose.connect("mongodb+srv://abhay-123:abhay-123@cluster0.tcbmx01.mongodb.net/feedthe_needy?retryWrites=true&w=majority", {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => {
